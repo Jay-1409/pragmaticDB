e5f522402c39d6be586f70fb5c6142e03320520b
Jay Shah <jay_shah@outlook.in>
2026-05-10 07:46:15 +0530

    changes: seperated tests into /tests, following srp

---
+++ include/tests.h
@@ -0,0 +1,6 @@
+#include<iostream>
+#include "buffer_pool_manager.h"
+class test {
+    public:
+    void TestBufferPoolManager();
+};
---
+++ src/page_data_manager.cpp
@@ -0,0 +1,5 @@
+#include<iostream>
+#include "../include/page_data_manager.h"
+void PageDataManager::test(){
+    std::cout << "Page data manager is responding" << std::endl;
+}
